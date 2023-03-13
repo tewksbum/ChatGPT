b50c70fce475f7d8b0f0a22ffe047fdb3cf4bef4
tewksbum <tewksbum@hotmail.com>
2023-03-13 17:33:06 -0400

    initial commit

---
+++ twitter-scraper.py
@@ -0,0 +1,16 @@
+import tweepy
+
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+user_tweets = api.user_timeline(screen_name="MikeReiss", count=10)
+
+for tweet in user_tweets:
+    print(tweet.text)
